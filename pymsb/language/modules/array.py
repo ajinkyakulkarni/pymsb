0dea0d5472aad292e13f19d7ed1c664da8697617
Simon Tang <Simon-Tang@users.noreply.github.com>
2015-12-18 20:38:38 -0500

    Added new Array class.

---
+++ pymsb/language/modules/array.py
@@ -0,0 +1,9 @@
+from pymsb.language.modules import utilities as py_msb_utils
+
+class Array:
+    def __init__(self):
+        pass
+
+    def IsArray(self, array):
+        # Returns "True" if the specified variable is an array, "False" otherwise.
+        return False  # TODO: implement Array methods
