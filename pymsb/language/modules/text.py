362f71739bdb43b1d3611f56e51eef21735c4ff7
Simon Tang <Simon-Tang@users.noreply.github.com>
2016-01-03 15:47:40 -0500

    Removed unnecessary string conversions in MSB Text module.

--- pymsb/language/modules/text.py
+++ pymsb/language/modules/text.py
@@ -1,34 +1,36 @@
-# TODO: add the superclass for MSB objects that will automatically convert function arguments to strings
-
-
 # noinspection PyPep8Naming,PyMethodMayBeStatic
 class Text:
     def Append(self, t1, t2):
-        return str(t1) + str(t2)
+        return t1 + t2
 
     def ConvertToLowerCase(self, t):
-        return str(t).lower()
+        return t.lower()
 
     def ConvertToUpperCase(self, t):
-        return str(t).upper()
+        return t.upper()
 
     def EndsWith(self, t, subt):
-        return str(str(t).endswith(str(subt)))
+        return str(t.endswith(subt))
 
     def GetCharacter(self, code):
-        return chr(int(code))
+        try:
+            return chr(int(code))
+        except:
+            return chr(0)
 
     def GetCharacterCode(self, ch):
-        return str(ord(ch))
+        if not ch:
+            return "0"
+        return str(ord(ch[0]))
 
     def GetIndexOf(self, t, subt):
         ind = t.find(subt)
         if ind == -1:
             return "0"
-        return ind
+        return str(ind)
 
     def GetLength(self, t):
-        return str(len(str(t)))
+        return str(len(t))
 
     def GetSubText(self, t, start, length):
         try:
@@ -36,15 +38,15 @@ class Text:
             l = int(float(length))
             if s < 0 or l < 0:  # Note: if length < 0 in MSB, program crashes
                 return ""
-            return str(t)[s:s+l]
+            return t[s:s+l]
         except:
             return ""
 
     def GetSubTextToEnd(self, t, start):
-        return self.GetSubText(t, start, len(str(t)))
+        return self.GetSubText(t, start, len(t))
 
     def IsSubText(self, t, subt):
-        return str(str(subt) in str(t))
+        return str(subt in t)
 
     def StartsWith(self, t, subt):
-        return str(str(subt).startswith(str(subt)))
+        return str(subt.startswith(subt))
