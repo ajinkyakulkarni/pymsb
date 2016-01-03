c980769677cb82fa1fce03a3e6581584f31ad99c
Simon Tang <Simon-Tang@users.noreply.github.com>
2016-01-03 15:37:57 -0500

    Refactored Math module code to contain Math class contents inside Math class definition, and Math.Pi is now a property.

--- pymsb/language/modules/math.py
+++ pymsb/language/modules/math.py
@@ -2,34 +2,38 @@ import math
 import random
 import pymsb.language.modules
 
-# TODO: find the differences in precision with original MSB?
-
-
-class Math:
-    pass
-
-
 def math_wrapper(func):
     return lambda self, *args: str(func(*(map(pymsb.language.modules.utilities.numericize, args))))
 
+# TODO: find the differences in precision with original MSB?
 
-Math.ArcCos = math_wrapper(math.acos)
-Math.ArcSin = math_wrapper(math.asin)
-Math.ArcTan = math_wrapper(math.atan)
-Math.Ceiling = math_wrapper(math.ceil)
-Math.Cos = math_wrapper(math.cos)
-Math.Floor = math_wrapper(math.floor)
-Math.GetDegrees = math_wrapper(math.degrees)
-Math.GetRadians = math_wrapper(math.radians)
-Math.GetRandomNumber = math_wrapper(lambda max_number: random.randint(1, max_number))  # inclusive on both ends
-Math.Log = math_wrapper(math.log10)
-Math.Max = math_wrapper(max)
-Math.Min = math_wrapper(min)
-Math.NaturalLog = math_wrapper(math.log)
-Math.Pi = math.pi  # TODO: decide if using this or Microsoft's string constant
-Math.Power = math_wrapper(math.pow)
-Math.Remainder = math_wrapper(lambda dividend, divisor: dividend % divisor)
-Math.Round = math_wrapper(round)
-Math.Sin = math_wrapper(math.sin)
-Math.SquareRoot = math_wrapper(math.sqrt)
-Math.Tan = math_wrapper(math.tan)
+
+# noinspection PyPep8Naming
+class Math:
+    """
+    The implementation of the MSB Math module.
+    """
+
+    @property
+    def Pi(self):
+        return str(math.pi)  # TODO: restrict precision level?
+
+    ArcCos = math_wrapper(math.acos)
+    ArcSin = math_wrapper(math.asin)
+    ArcTan = math_wrapper(math.atan)
+    Ceiling = math_wrapper(math.ceil)
+    Cos = math_wrapper(math.cos)
+    Floor = math_wrapper(math.floor)
+    GetDegrees = math_wrapper(math.degrees)
+    GetRadians = math_wrapper(math.radians)
+    GetRandomNumber = math_wrapper(lambda max_number: random.randint(1, max_number))  # inclusive on both ends
+    Log = math_wrapper(math.log10)
+    Max = math_wrapper(max)
+    Min = math_wrapper(min)
+    NaturalLog = math_wrapper(math.log)
+    Power = math_wrapper(math.pow)
+    Remainder = math_wrapper(lambda dividend, divisor: dividend % divisor)
+    Round = math_wrapper(round)
+    Sin = math_wrapper(math.sin)
+    SquareRoot = math_wrapper(math.sqrt)
+    Tan = math_wrapper(math.tan)
