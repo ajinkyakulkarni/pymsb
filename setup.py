812103efb5531cfd80c1e2377a26017bd107b92c
Simon Tang <Simon-Tang@users.noreply.github.com>
2016-01-07 00:15:50 -0500

    Added correct (hopefully) values for setuptools installation requirements for all 3 platforms.

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ import platform
 
 
 def find_pyuserinput_requirements():
-    return []
-
-    # FIXME: We need to figure out how to install the PyUserInput prerequisites automatically.
+    # TODO: Verify that these values work across all supported platforms.
     plat = platform.system()
     if plat == "Windows":
         return ["pywin32", "pyHook"]
     if plat == "Darwin":  # This is actually Mac OS X.
-        return ["Quartz", "AppKit"]
+        return ["pyobjc-framework-Quartz", "AppKit"]
     if plat == "Linux":
-        return ["Xlib"]
+        return ["python3-xlib"]
 
 setup(name='pymsb',
       author='Simon Tang',
